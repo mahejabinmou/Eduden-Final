f48e9682c0e3de7709c050c6d60faa9bee5204d4
dmemopract <dmemopract@gmail.com>
2024-09-23 03:35:07 -0600

    coursedetails route

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ClientSide/Home/Home";
 import "./index.css";
+import WhatsNew from "./ClientSide/Whatsnew/WhatsNew";
+import "swiper/css";
+import "swiper/css/pagination";
 import CourseDetails from "./ClientSide/CourseDetails/CourseDetails";
 
 const router = createBrowserRouter([
@@ -10,7 +13,12 @@ const router = createBrowserRouter([
     element: <Home></Home>,
   },
   {
-    path: "/coursedetails",
+    path: "/courseDetails",
+    element: <CourseDetails></CourseDetails>,
+  },
+  { path: "/whatsnew", element: <WhatsNew /> },
+  {
+    path: "/courseDetails",
     element: <CourseDetails></CourseDetails>,
   },
 ]);
