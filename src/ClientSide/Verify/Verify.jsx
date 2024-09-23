89f7b7a5e038f38aa4b09fc343c5656cffe004fb
dmemopract <dmemopract@gmail.com>
2024-09-23 03:39:35 -0600

    route added

---
+++ src/ClientSide/Verify/Verify.jsx
@@ -0,0 +1,9 @@
+const Verify = () => {
+  return (
+    <div>
+      <h1>This is Verify</h1>
+    </div>
+  );
+};
+
+export default Verify;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import WhatsNew from "./ClientSide/Whatsnew/WhatsNew";
 import "swiper/css";
 import "swiper/css/pagination";
 import CourseDetails from "./ClientSide/CourseDetails/CourseDetails";
+import AboutUs from "./ClientSide/AboutUs/AboutUs";
+import Verify from "./ClientSide/Verify/Verify";
 
 const router = createBrowserRouter([
   {
@@ -17,9 +19,14 @@ const router = createBrowserRouter([
     element: <CourseDetails></CourseDetails>,
   },
   { path: "/whatsnew", element: <WhatsNew /> },
+
   {
-    path: "/courseDetails",
-    element: <CourseDetails></CourseDetails>,
+    path: "/aboutUs",
+    element: <AboutUs></AboutUs>,
+  },
+  {
+    path: "/verify",
+    element: <Verify></Verify>,
   },
 ]);
 
