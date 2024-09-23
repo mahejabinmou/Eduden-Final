9c88f2c180adea24707ee2b866f4490c83f2ca8a
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-09-23 14:41:37 +0600

    add whatnew page

---
+++ src/ClientSide/WhatsNew/WhatsNew.jsx
@@ -0,0 +1,13 @@
+import UpcomingCourses from "../../components/WhatsNew/UpcomingCourses"
+
+const WhatsNew = () => {
+    return (
+        <>
+            <div className="font-sans mt-[150px] bg-[#010101] h-screen">
+                <UpcomingCourses />
+            </div>
+        </>
+    )
+}
+
+export default WhatsNew
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ClientSide/Home/Home";
-import FoundationCourse from "./ClientSide/CourseDetails/FoundationCourse";
-import "./index.css";
+import * as ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./ClientSide/Home/Home"
+import FoundationCourse from "./ClientSide/CourseDetails/FoundationCourse"
+import "./index.css"
+import WhatsNew from "./ClientSide/Whatsnew/WhatsNew"
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>,
-  },
-  {
-    path: "/foundationcourse",
-    element: <FoundationCourse></FoundationCourse>,
-  },
-]);
+    {
+        path: "/",
+        element: <Home></Home>,
+    },
+    {
+        path: "/foundationcourse",
+        element: <FoundationCourse></FoundationCourse>,
+    },
+    { path: "/whatsnew", element: <WhatsNew /> },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+    <RouterProvider router={router} />
+)
