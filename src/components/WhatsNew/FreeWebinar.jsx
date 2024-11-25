e48f39ca74ad37ebb26dab1e4bc2ec6c5d9dc81b
MaHim <mahimnxt23@gmail.com>
2024-11-25 13:00:01 +0600

    changed courseDetails, whatsNew, contactUs

--- src/components/WhatsNew/FreeWebinar.jsx
+++ src/components/WhatsNew/FreeWebinar.jsx
@@ -3,19 +3,19 @@ import FreeWebinarCard from "./FreeWebinarCard";
 import UpCmnTtle from "./UpCmnTtle";
 
 const FreeWebinar = () => {
-  return (
-    <div className="mt-[70px] md:mt-[100px] sectionGap">
-      <UpCmnTtle />
-      <h3 className="popularCategoryHead text-[20px] text-white md:text-[70px] font-bold font-gucina mt-2 text-center">
-        Upcoming Free Webinar
-      </h3>
-      <div className="webNarContainer mt-[50px] md:mt-[75px]">
-        {freeWebinar.map((webinar) => (
-          <FreeWebinarCard key={webinar.id} {...webinar} />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="mt-[70px] md:mt-[100px] sectionGap">
+			<UpCmnTtle />
+			<h3 className="popularCategoryHead text-white font-gucina mt-2 text-center">
+				Upcoming Free Webinar
+			</h3>
+			<div className="webNarContainer mt-[50px] md:mt-[75px]">
+				{freeWebinar.map((webinar) => (
+					<FreeWebinarCard key={webinar.id} {...webinar} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default FreeWebinar;
