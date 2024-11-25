92200df982e5785eed63cfbab9b328ff6a7822ef
MaHim <mahimnxt23@gmail.com>
2024-11-25 23:06:26 +0600

    added image on hover (AboutUsPage.jsx and OnlineCourse.jsx changed)

--- src/ClientSide/AboutUsPage/AboutUsPage.jsx
+++ src/ClientSide/AboutUsPage/AboutUsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import OurCollaboration from "../Home/OurCollaboration/OurCollaboration";
-import Testimonial from "../Home/Testimonial/Testimonial";
+// import Testimonial from "../Home/Testimonial/Testimonial";
 import Video from "../Home/Video/Video";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
@@ -9,21 +9,21 @@ import OnlineCourses from "./OnlineCourses/OnlineCourses";
 import TestimonialAbout from "./TestimonialAbout/TestimonialAbout";
 
 const AboutUsPage = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <div>
-      <Navbar></Navbar>
-      <AboutHero></AboutHero>
-      <OnlineCourses></OnlineCourses>
-      <Video></Video>
-      {/* <Testimonial></Testimonial> */}
-      <TestimonialAbout />
-      <OurCollaboration></OurCollaboration>
-      <Footer></Footer>
-    </div>
-  );
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+	return (
+		<div>
+			<Navbar></Navbar>
+			<AboutHero></AboutHero>
+			<OnlineCourses></OnlineCourses>
+			<Video></Video>
+			{/* <Testimonial></Testimonial> */}
+			<TestimonialAbout />
+			<OurCollaboration></OurCollaboration>
+			<Footer></Footer>
+		</div>
+	);
 };
 
 export default AboutUsPage;
