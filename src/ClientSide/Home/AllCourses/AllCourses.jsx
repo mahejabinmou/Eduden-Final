1f70725c8d01dbe09a3d3c98b731edef495e92ff
MaHim <mahimnxt23@gmail.com>
2024-12-01 17:53:37 +0600

    faq fix for eduden

--- src/ClientSide/Home/AllCourses/AllCourses.jsx
+++ src/ClientSide/Home/AllCourses/AllCourses.jsx
@@ -1,94 +1,205 @@
-// import { HiOutlineMinusSmall } from "react-icons/hi2";
-// import Course from "./Course";
-// import useFilterCategory from "../../../hooks/useFilterCategory";
+// // import { HiOutlineMinusSmall } from "react-icons/hi2";
+// // import Course from "./Course";
+// // import useFilterCategory from "../../../hooks/useFilterCategory";
+// // import { useState } from "react";
+// // import categories from "../../../constant/stateDummyData";
+
+// // const AllCourses = () => {
+// //   const [categoriesList, setCategoriesList] = useState({ ...categories });
+
+// //   const { category: currentCategory } = useFilterCategory(categoriesList);
+// //   const handleCategories = (category) => {
+// //     if (category === currentCategory) {
+// //       setCategoriesList({ ...categories });
+// //       return;
+// //     }
+// //     const updateCategory = { ...categoriesList };
+
+// //     for (let cat in updateCategory) {
+// //       if (cat === category) {
+// //         updateCategory[cat] = true;
+// //       } else {
+// //         updateCategory[cat] = false;
+// //       }
+// //     }
+// //     setCategoriesList(updateCategory);
+// //   };
+// //   return (
+// //     <div className="sectionGap text-[#FFFFFF]">
+// //       <div>
+// //         <h1 className="joinClanPara mb-[25px]  flex items-center justify-center text-[#A8FF57]">
+// //           <span>
+// //             <HiOutlineMinusSmall />{" "}
+// //           </span>
+// //           Popular Categories{" "}
+// //           <span>
+// //             <HiOutlineMinusSmall />{" "}
+// //           </span>
+// //         </h1>
+// //         <h1 className="joinClanHead mb-[80px] text-center">
+// //           {" "}
+// //           All <span className="border-b-4 border-[#A8FF57]">Courses</span>{" "}
+// //         </h1>
+// //       </div>
+
+// //       <div className="hidden lg:block flex text-center items-center flex-row lg:gap-x-[20px] gap-x-[5px] text-white group">
+// //         <button
+// //           onClick={() => handleCategories("Show All")}
+// //           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// //         >
+// //           Show All
+// //         </button>
+// //         <button
+// //           onClick={() => handleCategories("Graphicis Design")}
+// //           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// //         >
+// //           Graphicis Design
+// //         </button>
+// //         <button
+// //           onClick={() => handleCategories("Ux/Ui Design")}
+// //           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// //         >
+// //           Ux/Ui Design
+// //         </button>
+// //         <button
+// //           onClick={() => handleCategories("Web Development")}
+// //           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// //         >
+// //           Web Development
+// //         </button>
+// //         <button
+// //           onClick={() => handleCategories("Mobile App Development")}
+// //           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#553030]"
+// //         >
+// //           Mobile App Development
+// //         </button>
+// //         <button
+// //           onClick={() => handleCategories("Programming")}
+// //           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// //         >
+// //           Programming
+// //         </button>
+// //         <button
+// //           onClick={() => handleCategories("Ethical Hacking")}
+// //           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// //         >
+// //           Ethical Hacking
+// //         </button>
+// //       </div>
+// //       <Course currentCategory={currentCategory} />
+// //     </div>
+// //   );
+// // };
+
+// // export default AllCourses;
+
 // import { useState } from "react";
+// import { HiOutlineMinusSmall } from "react-icons/hi2";
 // import categories from "../../../constant/stateDummyData";
+// import useFilterCategory from "../../../hooks/useFilterCategory";
+// import Course from "./Course";
 
 // const AllCourses = () => {
-//   const [categoriesList, setCategoriesList] = useState({ ...categories });
+// 	const [categoriesList, setCategoriesList] = useState({ ...categories });
 
-//   const { category: currentCategory } = useFilterCategory(categoriesList);
-//   const handleCategories = (category) => {
-//     if (category === currentCategory) {
-//       setCategoriesList({ ...categories });
-//       return;
-//     }
-//     const updateCategory = { ...categoriesList };
+// 	const { category: currentCategory } = useFilterCategory(categoriesList);
+// 	const handleCategories = (category) => {
+// 		if (category === currentCategory) {
+// 			setCategoriesList({ ...categories });
+// 			return;
+// 		}
+// 		const updateCategory = { ...categoriesList };
 
-//     for (let cat in updateCategory) {
-//       if (cat === category) {
-//         updateCategory[cat] = true;
-//       } else {
-//         updateCategory[cat] = false;
-//       }
-//     }
-//     setCategoriesList(updateCategory);
-//   };
-//   return (
-//     <div className="sectionGap text-[#FFFFFF]">
-//       <div>
-//         <h1 className="joinClanPara mb-[25px]  flex items-center justify-center text-[#A8FF57]">
-//           <span>
-//             <HiOutlineMinusSmall />{" "}
-//           </span>
-//           Popular Categories{" "}
-//           <span>
-//             <HiOutlineMinusSmall />{" "}
-//           </span>
-//         </h1>
-//         <h1 className="joinClanHead mb-[80px] text-center">
-//           {" "}
-//           All <span className="border-b-4 border-[#A8FF57]">Courses</span>{" "}
-//         </h1>
-//       </div>
+// 		for (let cat in updateCategory) {
+// 			if (cat === category) {
+// 				updateCategory[cat] = true;
+// 			} else {
+// 				updateCategory[cat] = false;
+// 			}
+// 		}
+// 		setCategoriesList(updateCategory);
+// 	};
+// 	return (
+// 		<div className="sectionGap text-[#FFFFFF]">
+// 			<div>
+// 				<h1 className="testimonialSub mb-[20px]  flex items-center justify-center text-[#A8FF57]">
+// 					<span>
+// 						<HiOutlineMinusSmall />{" "}
+// 					</span>
+// 					Popular Categories{" "}
+// 					<span>
+// 						<HiOutlineMinusSmall />{" "}
+// 					</span>
+// 				</h1>
+// 				<h1 className="popularCategoryHead mb-[35px] text-center">
+// 					{" "}
+// 					All <span className="BorderGlobal border-[#A8FF57]">
+// 						Courses
+// 					</span>{" "}
+// 				</h1>
+// 			</div>
 
-//       <div className="hidden lg:block flex text-center items-center flex-row lg:gap-x-[20px] gap-x-[5px] text-white group">
-//         <button
-//           onClick={() => handleCategories("Show All")}
-//           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-//         >
-//           Show All
-//         </button>
-//         <button
-//           onClick={() => handleCategories("Graphicis Design")}
-//           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-//         >
-//           Graphicis Design
-//         </button>
-//         <button
-//           onClick={() => handleCategories("Ux/Ui Design")}
-//           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-//         >
-//           Ux/Ui Design
-//         </button>
-//         <button
-//           onClick={() => handleCategories("Web Development")}
-//           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-//         >
-//           Web Development
-//         </button>
-//         <button
-//           onClick={() => handleCategories("Mobile App Development")}
-//           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#553030]"
-//         >
-//           Mobile App Development
-//         </button>
-//         <button
-//           onClick={() => handleCategories("Programming")}
-//           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-//         >
-//           Programming
-//         </button>
-//         <button
-//           onClick={() => handleCategories("Ethical Hacking")}
-//           className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-//         >
-//           Ethical Hacking
-//         </button>
-//       </div>
-//       <Course currentCategory={currentCategory} />
-//     </div>
-//   );
+// 			<div className="hidden lg:block flex text-center items-center flex-row lg:gap-x-[20px] gap-x-[5px] space-x-4 space-y-4 text-white group lg:mb-[50px]">
+// 				<button
+// 					onClick={() => handleCategories("Show All")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					Show All
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("Cyber Security")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					Cyber Security
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("Graphics Design")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					Graphics Design
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("UI/UX Design")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					UI/UX Design
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("Web Development")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					Web Development
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("Mobile App Development")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#553030]"
+// 				>
+// 					Mobile App Development
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("Programming")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					Programming
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("Ethical Hacking")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					Ethical Hacking
+// 				</button>
+// 				<button
+// 					onClick={() => handleCategories("Data Science and AI")}
+// 					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
+// 				>
+// 					Data Science and AI
+// 				</button>
+// 			</div>
+// 			<div className="mt-[35px]">
+// 				<Course currentCategory={currentCategory} />
+// 			</div>
+// 		</div>
+// 	);
 // };
 
 // export default AllCourses;
@@ -103,6 +214,7 @@ const AllCourses = () => {
 	const [categoriesList, setCategoriesList] = useState({ ...categories });
 
 	const { category: currentCategory } = useFilterCategory(categoriesList);
+
 	const handleCategories = (category) => {
 		if (category === currentCategory) {
 			setCategoriesList({ ...categories });
@@ -119,10 +231,11 @@ const AllCourses = () => {
 		}
 		setCategoriesList(updateCategory);
 	};
+
 	return (
 		<div className="sectionGap text-[#FFFFFF]">
 			<div>
-				<h1 className="testimonialSub mb-[20px]  flex items-center justify-center text-[#A8FF57]">
+				<h1 className="testimonialSub mb-[20px] flex items-center justify-center text-[#A8FF57]">
 					<span>
 						<HiOutlineMinusSmall />{" "}
 					</span>
@@ -139,62 +252,25 @@ const AllCourses = () => {
 				</h1>
 			</div>
 
+			{/* Category Buttons */}
 			<div className="hidden lg:block flex text-center items-center flex-row lg:gap-x-[20px] gap-x-[5px] space-x-4 space-y-4 text-white group lg:mb-[50px]">
-				<button
-					onClick={() => handleCategories("Show All")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					Show All
-				</button>
-				<button
-					onClick={() => handleCategories("Cyber Security")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					Cyber Security
-				</button>
-				<button
-					onClick={() => handleCategories("Graphics Design")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					Graphics Design
-				</button>
-				<button
-					onClick={() => handleCategories("UI/UX Design")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					UI/UX Design
-				</button>
-				<button
-					onClick={() => handleCategories("Web Development")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					Web Development
-				</button>
-				<button
-					onClick={() => handleCategories("Mobile App Development")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#553030]"
-				>
-					Mobile App Development
-				</button>
-				<button
-					onClick={() => handleCategories("Programming")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					Programming
-				</button>
-				<button
-					onClick={() => handleCategories("Ethical Hacking")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					Ethical Hacking
-				</button>
-				<button
-					onClick={() => handleCategories("Data Science and AI")}
-					className="allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101]"
-				>
-					Data Science and AI
-				</button>
+				{Object.keys(categoriesList).map((category) => (
+					<button
+						key={category}
+						onClick={() => handleCategories(category)}
+						className={`allCoursesBtn rounded-[50px] hover:bg-[#A8FF57] hover:text-[#010101] 
+							${
+								currentCategory === category
+									? "ring-2 ring-[#A8FF57] text-[#A8FF57] font-bold"
+									: ""
+							}`}
+					>
+						{category}
+					</button>
+				))}
 			</div>
+
+			{/* Courses */}
 			<div className="mt-[35px]">
 				<Course currentCategory={currentCategory} />
 			</div>
